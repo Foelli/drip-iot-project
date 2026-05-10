a16639993b9a6b76a431bbde45cb884e1061c211
Simon Felhofer <felhofer.simon@gmail.com>
2026-05-10 12:50:59 +0200

    feat(db): add exposed table + postgres repository + config

---
+++ ranelle-backend/src/main/kotlin/plant/data/database/PlantTable.kt
@@ -0,0 +1,15 @@
+package plant.data.database
+
+import org.jetbrains.exposed.v1.core.Table
+
+object PlantsTable : Table("plants") {
+    val id = integer("id")
+    val apiId = integer("api_id")
+    val commonName = text("common_name").nullable()
+    val scientificName = text("scientific_name").nullable()
+    val customName = text("custom_name").nullable()
+    val thumbnailUrl = text("thumbnail_url").nullable()
+    val description = text("description").nullable()
+
+    override val primaryKey = PrimaryKey(id)
+}
