9eb3eeea9cd4db1a05aa40e7216a684998a4ab36
Simon Felhofer <felhofer.simon@gmail.com>
2026-05-11 21:51:12 +0200

    chore(db): align schema and config

--- ranelle-backend/src/main/kotlin/plant/data/database/PlantTable.kt
+++ ranelle-backend/src/main/kotlin/plant/data/database/PlantTable.kt
@@ -3,7 +3,7 @@ package plant.data.database
 import org.jetbrains.exposed.v1.core.Table
 
 object PlantsTable : Table("plants") {
-    val id = integer("id")
+    val id = integer("id").autoIncrement()
     val apiId = integer("api_id")
     val commonName = text("common_name").nullable()
     val scientificName = text("scientific_name").nullable()
